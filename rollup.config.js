ad8dc00311d1db858fb1c3cfde70cf09eb1ab57e
wilhelm martinez <wilhelmpaulm@gmail.com>
2020-05-27 00:26:25 +0800

    now working offline

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import merge from 'deepmerge';
+import copy from "rollup-plugin-copy";
 // use createSpaConfig for bundling a Single Page App
-import { createSpaConfig } from '@open-wc/building-rollup';
+import { createSpaConfig } from "@open-wc/building-rollup";
+import merge from "deepmerge";
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -14,16 +15,33 @@ const baseConfig = createSpaConfig({
   // legacyBuild: true,
 
   // development mode creates a non-minified build for debugging or development
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  developmentMode: process.env.ROLLUP_WATCH === "true",
 
   // set to true to inject the service worker registration into your index.html
-  injectServiceWorker: false,
+  injectServiceWorker: true,
 });
 
 export default merge(baseConfig, {
   // if you use createSpaConfig, you can use your index.html as entrypoint,
   // any <script type="module"> inside will be bundled by rollup
-  input: './index.html',
+  // input: "./index.html",
+  input: "./index.html",
+  output: {
+    dir: "dist",
+    format: "cjs",
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: "./404.html", dest: "dist" },
+        { src: "./css/**/*", dest: "dist/css" },
+        { src: "./img/**/*", dest: "dist/img" },
+        { src: "./manifest.json", dest: "dist" },
+        { src: "./browserconfig.xml", dest: "dist" },
+        { src: "./custom-elements.json", dest: "dist" },
+      ],
+    }),
+  ],
 
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
--- src/twenty-one.js
+++ src/twenty-one.js
@@ -1,5 +1,8 @@
 import { TodoTable } from "./components/TodoTable";
 import { TwentyOne } from "./TwentyOne.js";
 
+// import "./../img/icon-512.png";
+// import "./../css/vendor/bootstrap.min.css";
+
 customElements.define("todo-table", TodoTable);
 customElements.define("twenty-one", TwentyOne);
